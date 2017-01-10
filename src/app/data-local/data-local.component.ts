e6719f36d9777b50cb10e95ae9d1abda736c33d0
Jorge <jorgemarquez2222@gmail.com>
2017-01-10 15:45:58 -0400

    Optimizacion

--- src/app/data-local/data-local.component.ts
+++ src/app/data-local/data-local.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef } from '@angular/core';
 import { Miembros } from '../interfaces/interfaceMiembros';
 import { ServicelocalService } from '../services/servicelocal.service';
 import { AngularFire, FirebaseObjectObservable, FirebaseListObservable,AuthProviders, AuthMethods } from 'angularfire2';
 import { Router, ActivatedRoute } from '@angular/router';
 import {SpinnerService} from '../services/spinner.service';
+declare var jQuery:any;
 
 @Component({
   selector: 'app-data-local',
@@ -13,11 +14,13 @@ import {SpinnerService} from '../services/spinner.service';
 export class DataLocalComponent implements OnInit {
 
   miembros: Miembros[];
+  detallesMiembro : Miembros;
   constructor(
     private _servicioLocal: ServicelocalService,
      public af: AngularFire,
    private router:Router,
-   private _spinner : SpinnerService
+   private _spinner : SpinnerService,
+   private _elRef: ElementRef
   ) { }
 
   getData(){
@@ -34,8 +37,15 @@ export class DataLocalComponent implements OnInit {
   }
 
   ngOnInit(){
+    jQuery(this._elRef.nativeElement).find('.modal').modal();
     this._spinner.start();
     this.getData();
+  }
+
+  detalles(miembro : Miembros){
+    console.log(miembro)
+   this.detallesMiembro = miembro;
+
   }
 
 }
