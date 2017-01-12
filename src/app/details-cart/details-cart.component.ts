a3bb0ea47dd220bb8d48363be57edaa414803765
Jorge <jorgemarquez@gmail.com>
2017-01-12 00:18:44 -0430

    Optimizacion

--- src/app/details-cart/details-cart.component.ts
+++ src/app/details-cart/details-cart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {CartService } from '../services/cart.service';
 import {Product} from '../interfaces/interfaceProduct';
 import {ProductCart} from '../interfaces/interfaceProductCart';
+declare var jQuery:any;
+
 @Component({
   selector: 'app-details-cart',
   templateUrl: './details-cart.component.html',
@@ -19,6 +21,19 @@ export class DetailsCartComponent implements OnInit {
     this.details = this._serviceCart.getDetails();
     console.log(this.products);
     console.log(this.details);
+
+    
+  jQuery('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+
   }
 
 }
